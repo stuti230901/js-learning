4a80ae0133ca92c2dc12541e0006c58f23b1f6b5
stuti230901 <srivastavastuti230109@gmail.com>
2025-05-17 20:24:48 +0000

    dataTypes

---
+++ basics_01/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // shows that the JS should follow the newer version.
+
+// PRIMITIVE - Types of data types
+// 1. Number -> range upto 2 to the power of 52.
+// 2. bigint -> when handling large numbers.
+// 3. string -> ""
+// 4. boolean -> true/false
+// 5. null -> indicates empty value. it does not have a type.
+// 6. undefined -> it is when a variable is not defined yet.
+// 7. symbol
+
+
+// object
+
+// typeof - tells the type of the variable/value.
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
